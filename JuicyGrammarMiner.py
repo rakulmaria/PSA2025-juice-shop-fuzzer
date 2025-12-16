a8cee1fa499ec96a1354d69dcd5b5123be1b5c23
rakulmaria <rakulmaria@hotmail.com>
2025-12-16 10:01:12 +0100

    add XSS attack with soundcloud music

--- JuicyGrammarMiner.py
+++ JuicyGrammarMiner.py
@@ -81,7 +81,8 @@ SQLI_GRAMMAR: Grammar = ({
         UNEXPLORED_STATE: [""],
         FINAL_STATE: [""],
 
-        "<XSS>": ["<left>iframe src=\"javascript:alert(<single-quote>xss<single-quote>)\"<right>"],
+        "<XSS>": ["<left>iframe src=\"javascript:alert(<single-quote>xss<single-quote>)\"<right>",
+                  "<left>iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true\"\<right>"],
         "<left>": ["<"],
         "<right>": [">"],        
 
