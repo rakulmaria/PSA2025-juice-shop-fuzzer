89e913642d2e7ce798b38f2f8338edace02c1bfd
Lotte <lottejd@gmail.com>
2025-12-17 17:20:56 +0100

    Fix runner

--- JuicyRunner.py
+++ JuicyRunner.py
@@ -106,28 +106,27 @@ class JuicyRunner(GUIRunner):
             alert.accept()
             WebDriverWait(self.driver, timeout=5, poll_frequency=1).until_not(EC.alert_is_present())
             return "XSS-alert", self.FAIL
-        except (TimeoutException, NoAlertPresentException):
+        except (TimeoutException, NoAlertPresentException) as e:
             pass
 
-        try:
-            elem = self.driver.find_element(By.ID, "xss-soundcloud")
-            if (elem) : return "XSS-soundcloud", self.FAIL
-        except NoSuchElementException:
-            pass
+        elems = self.driver.find_elements(By.ID, "xss-soundcloud")
+        if (len(elems) > 0): 
+            return "XSS-soundcloud", self.FAIL
 
         return "", self.PASS
     
     def has_error_sqli(self) -> Tuple[str, str] :
-        logged_in = not ("login" in self.driver.current_url)
-        if logged_in:
+        basket_elems = self.driver.find_elements(By.XPATH, "//*[contains(text(),'Your Basket')]")
+        if len(basket_elems)>0:
             return "logged in", self.FAIL 
-        
 
         errors = self.driver.find_elements(By.CLASS_NAME, "error")
     
         if (len(errors) > 0):
             if (errors[0].text != ACCEPTABLE_ERROR_MSG):
                 return errors[0].text, self.FAIL         
+        
+        return "", self.PASS
     
     def oracle(self) -> Tuple[str,str]:
         xss = self.has_error_xss()
@@ -180,10 +179,10 @@ class JuicyRunner(GUIRunner):
         #TODO: fix action instead of this hack
         if "submit('loginButton')" in inp:
             input = inp.split("submit('loginButton')")[0]+"submit('loginButton')"
-        elif "search('<iframe src=\"javascript:alert" in inp:
+        elif "javascript:alert" in inp:
             input = "search('<iframe src=\"javascript:alert(\\'xss\\')\">')"
-        elif "search('<iframe id=\"xss-soundcloud\"" in inp:
-            input = "search('<iframe id=\"xss-soundcloud\" width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true\">)"
+        elif "soundcloud" in inp:
+            input = "search('<iframe id=\"xss-soundcloud\" width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true\">')"
         else:
             input = inp
 
